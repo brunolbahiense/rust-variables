769f964de1ddbd8a86cfe65a0bdd185f97080315
Bruno Luis Bahiense <brunobahiense@saltsystems.com>
2022-11-16 18:01:39 -0300

    more variables created

--- main.rs
+++ main.rs
@@ -1,4 +1,19 @@
+// u - significam valores inteiros não negativos (unsigned Integer) 
+// i - significa um valor inteiro (integer)
+// f - significa um valor em ponto flutuante
 fn main(){
-    let variavel:u8 = 129;
-    println!(" variavel = {}", variavel)
+    let variavel:u8 = 129; 
+    println!(" variavel i = {}", variavel);
+
+    let variavel2:i32 = 45;
+    println!(" variavel u = {}", variavel2);
+
+    let decimal:f32 = 4.5;
+    println!(" decimal  = {}", decimal);
+
+    let mut boolean:bool = true;
+    println!(" boolean  = {}", boolean);
+
+    let letra:char = 'B';
+    println!(" letra  = {}", letra);
 }
